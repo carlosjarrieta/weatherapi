1ec3493d82c1901a10779ea8c531de32ad15f724
Carlos Arrieta <carlosj.arrieta@gmail.com>
2023-03-09 11:02:53 -0500

    refactor Api Base

--- app/services/api/wearther_api/base.rb
+++ app/services/api/wearther_api/base.rb
@@ -4,7 +4,7 @@ module Api
       require "uri"
       require "net/http"
 
-      attr_accessor :url_string, :request, :https, :response, :params
+      attr_accessor :city_name
 
       API_URL = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]['api_url']
       API_KEY = YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]['api_key']
