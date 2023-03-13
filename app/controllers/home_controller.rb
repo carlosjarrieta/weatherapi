660088e6c5019d208fb7b7408ad7a01b81bfd831
Carlos Arrieta <carlosj.arrieta@gmail.com>
2023-03-13 14:28:44 -0500

    store the city in rails cache

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,10 +9,7 @@ class HomeController < ApplicationController
   def search
     if params[:name_city].present?
       name_search = params[:name_city].downcase
-      @city = City.where("name_search like ?", "%#{name_search}%").first
-      if @city.nil?
-        @city = CityConverterHelper::Convert.new(Api::WeartherApi::Current.new(name_search), name_search).call
-      end
+      @city = get_city_on_cache(name_search)
       if @city.nil?
         ActionCable.server.broadcast "city_channel", {
           message: I18n.t('dashboard.city_nil'),
@@ -22,12 +19,30 @@ class HomeController < ApplicationController
       end
     end
 
-    get_my_cities
     respond_to do |format|
+      get_my_cities
       format.turbo_stream do
         render layout: false
       end
     end
+  end
+
+  private
+
+  def get_city_on_cache(name_search)
+    #1.primero la busco en el cache de redis
+    city = Rails.cache.read(name_search)
+    if city.nil?
+      #2. Si ya no esta disponible en redis, la busco el la DB PostgreSql
+      city = City.where("name_search like ?", "%#{name_search}%").first
+      #3. Si tampoco esta en PostgreSql, entonces la busco en el Api de weatherApi y la cacheo en redis por 12 horas
+      if city.nil?
+        city = Rails.cache.fetch(name_search, expires_in: 12.hours) do
+          CityConverterHelper::Convert.new(Api::WeartherApi::Current.new(name_search), name_search).call
+        end
+      end
+    end
+    city
   end
 
   def get_my_cities
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,4 +79,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
+
+  config.cache_store = :redis_cache_store, { url: YAML.load_file("#{Rails.root}/config/application.yml")[Rails.env]['redis_url'] }
+
 end
